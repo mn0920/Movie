168c52ccfbd34eadf91ba3d6df26ce010792481d
Unknown <dlalsdk12321@naver.com>
2018-12-21 09:15:49 +0900

    관리자 컨트롤러에 영화,배우,감독,장르 찾기, 추가 페이지만 등록

--- src/main/java/kr/min/movie/controller/AdminController.java
+++ src/main/java/kr/min/movie/controller/AdminController.java
@@ -19,31 +19,89 @@ public class AdminController {
   private AccountService accountService;
   @Autowired
   private BoardService boardService;
-  @Autowired
-  private BoardVo boardVo;
-  @Autowired
-  private BoardDao boardDao;
 
   @RequestMapping(value = "/addM", method = RequestMethod.GET)
-  public String addGet(BoardVo boardVo, Model model) {
-    return "add/addMovie";
+  public String addMovieGet(BoardVo boardVo, Model model) {
+    return "admin/movieManagement";
   }
   
   @RequestMapping(value = "/addM", method = RequestMethod.POST)
-  public String addPost(Model model) {
-    String actorList = boardVo.getActor_list(); // 스트링 문자 POWER
-    String[] actor; // 스트링을 담을 배열
-
-    actor = actorList.split(","); // 배열에 한글자씩 저장하기
-
-    for (int i = 0; i < actor.length; i++) { // 출력
-      System.out.println(actor[i]);
-      boardDao.searchActor(actor);
-    }
-    
-    /*model.*/
-    
-    return "add/addMovie";
+  public String addMoviePost(Model model, BoardVo boardVo, String[] actor) {
+    return "admin/movieManagement";
   }
+  
+
+  @RequestMapping(value = "/MM/seaM", method = RequestMethod.GET)
+  public String searchMovieGet(BoardVo boardVo, Model model) {
+    return "admin/search/searchMovie";
+  }
+  
+  @RequestMapping(value = "/MM/seaM", method = RequestMethod.POST)
+  public String searchMoviePost(Model model, BoardVo boardVo, String[] actor) {
+/*      boardService.searchActor(actor);*/
+    return "admin/search/searchMovie";
+  }
+
+  
+  
+  @RequestMapping(value = "/MM/addA", method = RequestMethod.GET)
+  public String addActorGet(BoardVo boardVo, Model model) {
+    return "admin/add/addActor";
+  }
+  
+  @RequestMapping(value = "/MM/addA", method = RequestMethod.POST)
+  public String addActorPost(Model model, BoardVo boardVo, String[] actor) {
+    return "admin/add/addActor";
+  }
+
+  
+  @RequestMapping(value = "/MM/seaA", method = RequestMethod.GET)
+  public String searchActorGet(BoardVo boardVo, Model model) {
+    return "admin/search/searchActor";
+  }
+  
+  @RequestMapping(value = "/MM/seaA", method = RequestMethod.POST)
+  public String searchActorPost(Model model, BoardVo boardVo, String[] actor) {
+    return "admin/search/searchActor";
+  }
+
+  
+  
+  
+  @RequestMapping(value = "/MM/addD", method = RequestMethod.GET)
+  public String addDirectorGet(BoardVo boardVo, Model model) {
+    return "admin/add/addDirector";
+  }
+  
+  @RequestMapping(value = "/MM/addD", method = RequestMethod.POST)
+  public String addDirectorPost(Model model, BoardVo boardVo, String[] actor) {
+    return "admin/add/addDirector";
+  }
+
+  
+  
+  @RequestMapping(value = "/MM/seaD", method = RequestMethod.GET)
+  public String searchDirectorGet(BoardVo boardVo, Model model) {
+    return "admin/search/searchDirector";
+  }
+  
+  @RequestMapping(value = "/MM/seaD", method = RequestMethod.POST)
+  public String searchDirectorPost(Model model, BoardVo boardVo, String[] actor) {
+    return "admin/search/searchDirector";
+  }
+
+  
+  
+  
+  @RequestMapping(value = "/MM/seaG", method = RequestMethod.GET)
+  public String searchGenreGet(BoardVo boardVo, Model model) {
+    return "admin/search/searchGenre";
+  }
+  
+  @RequestMapping(value = "/MM/seaG", method = RequestMethod.POST)
+  public String searchGenrePost(Model model, BoardVo boardVo, String[] actor) {
+    return "admin/search/searchGenre";
+  }
+
 
 }
